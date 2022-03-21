8166471d8bac9cd5affa61be8ec159b466396ad2
Causedmass957 <arnbristerdjr@gmail.com>
2022-03-21 17:42:32 -0400

    more security stuff and started work on dog model

--- SaveADawg/src/main/java/com/example/models/Dog.java
+++ SaveADawg/src/main/java/com/example/models/Dog.java
@@ -1,5 +1,33 @@
 package com.example.models;
 
+import java.util.Date;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+
+@Entity
+@Table(name="Dog")
 public class Dog {
+	
+	@Id
+	@Column(name="dog_id")//this tells hibernate this field is a column and the column name it should be mapped to	
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private int dogId;
+	
+	@Column(name="dateofbirth", nullable=false)
+	private Date dateOfBirth;
+	
+	@JoinColumn(name="breed_id")
+	@ManyToOne(cascade=CascadeType.MERGE, fetch = FetchType.EAGER)
+	private DogBreed dogBreed;
 
 }
