1e2dbcb2a3506b8202d76d37dc5a23f731ff925f
Prabin <rprabin046@gmail.com>
2022-03-21 20:45:53 -0400

    Dog Model updated

--- SaveADawg/src/main/java/com/example/models/Dog.java
+++ SaveADawg/src/main/java/com/example/models/Dog.java
@@ -19,9 +19,12 @@ import javax.persistence.Table;
 public class Dog {
 	
 	@Id
-	@Column(name="dog_id")//this tells hibernate this field is a column and the column name it should be mapped to	
+	@Column(name="dog_id", updatable = false, nullable = false, unique = true)//this tells hibernate this field is a column and the column name it should be mapped to	
 	@GeneratedValue(strategy=GenerationType.IDENTITY)
 	private int dogId;
+	
+	@Column(name="name")
+	private String name;
 	
 	@Column(name="dateofbirth", nullable=false)
 	private Date dateOfBirth;
@@ -29,5 +32,76 @@ public class Dog {
 	@JoinColumn(name="breed_id")
 	@ManyToOne(cascade=CascadeType.MERGE, fetch = FetchType.EAGER)
 	private DogBreed dogBreed;
+	
+	@JoinColumn(name="user_id")
+	@ManyToOne(cascade=CascadeType.MERGE, fetch = FetchType.EAGER)
+	private User dogOwner;
+	
+	public Dog() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public Dog(String name, Date dateOfBirth, DogBreed dogBreed, User dogOwner) {
+		super();
+		this.name = name;
+		this.dateOfBirth = dateOfBirth;
+		this.dogBreed = dogBreed;
+		this.dogOwner = dogOwner;
+	}
+	
+	public Dog(int dogId, String name, Date dateOfBirth, DogBreed dogBreed, User dogOwner) {
+		super();
+		this.dogId = dogId;
+		this.name = name;
+		this.dateOfBirth = dateOfBirth;
+		this.dogBreed = dogBreed;
+		this.dogOwner = dogOwner;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Date getDateOfBirth() {
+		return dateOfBirth;
+	}
+
+	public void setDateOfBirth(Date dateOfBirth) {
+		this.dateOfBirth = dateOfBirth;
+	}
+
+	public DogBreed getDogBreed() {
+		return dogBreed;
+	}
+
+	public void setDogBreed(DogBreed dogBreed) {
+		this.dogBreed = dogBreed;
+	}
+
+	public User getDogOwner() {
+		return dogOwner;
+	}
+
+	public void setDogOwner(User dogOwner) {
+		this.dogOwner = dogOwner;
+	}
+
+	public int getDogId() {
+		return dogId;
+	}
+
+	@Override
+	public String toString() {
+		return "Dog [dogId=" + dogId + ", name=" + name + ", dateOfBirth=" + dateOfBirth + ", dogBreed=" + dogBreed
+				+ ", dogOwner=" + dogOwner + "]";
+	}
+	
+	
+	
+	
 
 }
