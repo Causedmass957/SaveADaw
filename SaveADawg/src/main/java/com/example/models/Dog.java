c5274f5336a3fcc6499372ad79cd118f59d16c77
Cole A. Kendall <mrcoole7890@outlook.com>
2022-04-01 19:53:30 -0400

    remove @ManyToOne dog to breed relationship method

--- SaveADawg/src/main/java/com/example/models/Dog.java
+++ SaveADawg/src/main/java/com/example/models/Dog.java
@@ -25,9 +25,4 @@ public class Dog {
 	
 	@Column(name="dateofbirth", nullable=false)
 	private Date dateOfBirth;
-	
-	@JoinColumn(name="breed_id")
-	@ManyToOne(cascade=CascadeType.MERGE, fetch = FetchType.EAGER)
-	private DogBreed dogBreed;
-
 }
